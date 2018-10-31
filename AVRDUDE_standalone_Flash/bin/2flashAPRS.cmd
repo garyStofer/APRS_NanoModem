5695fbe5cd393048cd8545d4a265c7b8fa0d0bbb
garyStofer <garyStofer@gmail.com>
2018-10-30 18:25:48 -0700

    added a local CMD window shortcut so that one can observe the 2flashAPRS output

--- AVRDUDE_standalone_Flash/bin/2flashAPRS.cmd
+++ AVRDUDE_standalone_Flash/bin/2flashAPRS.cmd
@@ -1,1 +1,1 @@
-avrdude -v -p m328p -c arduino -PCOM8 -b57600   -U flash:w:..\..\Software\MicroAPRS\precompiled\microaprs-5v-ss-latest.hex:i
+avrdude -v -p m328p -c arduino -PCOM4 -b57600   -U flash:w:..\..\Software\MicroAPRS\precompiled\microaprs-5v-ss-latest.hex:i
